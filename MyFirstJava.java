a4b54d9fa3abf309911ec371ff38680638290aa7
Sanchari <gullugreat@gmail.com>
2023-08-09 02:11:39 +0800

    first java file commit

---
+++ MyFirstJava.java
@@ -0,0 +1,5 @@
+public class MyFirstJava{ //name of first class should match name of file 
+    public static void main (String []args){ //public is an access modifier, static allows main to be called without instantiating the object, void return type
+        System.out.println("My First Java program...");
+    }
+}
